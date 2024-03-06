6d79b9b81e620af55238f64646440ff9cf3bd9e1
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-03-06 16:47:55 +0100

    added elements to the page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Spesa</title>
 </head>
 <body>
     <h1>Hello world</h1>
+    @forelse ($ingredients as $ingredient)
+    <p>{{$ingredient}}  </p>
+    @empty
+    <h3>non ci sono ingredienti</h3>
+    @endforelse
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $ingredients = ['Pollo','Cipolla','Banane'];
+    return view('home', compact('ingredients'));
 });
+
+
